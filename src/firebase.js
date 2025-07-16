328a4a124c8b4a66b37d3e0a8506f344955f854f
sanith-b <05sanith@gmail.com>
2025-07-17 00:03:39 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyADXp3zaVe4l2tUjWlZ7Af_VQ1LJJ7FIyY",
+authDomain: "webapp-661ff.firebaseapp.com",
+projectId: "webapp-661ff",
+storageBucket: "webapp-661ff.firebasestorage.app",
+messagingSenderId: "456707938008",
+appId: "1:456707938008:web:3a71f517eb2c7fc15aa9ad"
+
 };
 
 const app = initializeApp(firebaseConfig);
